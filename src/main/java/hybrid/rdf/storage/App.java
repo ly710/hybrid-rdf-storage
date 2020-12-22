88a91682c8f070d412b37632b6c545eb2e8cf56c
ly710 <ly710@outlook.com>
2020-12-22 20:21:44 +0800

    Mod fix

---
+++ src/main/java/hybrid/rdf/storage/App.java
@@ -0,0 +1,13 @@
+package hybrid.rdf.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"hybrid.rdf.storage"})
+public class App  {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
+
